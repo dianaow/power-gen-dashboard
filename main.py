7b5a6283da7e3774a1753827b21f1b8729941c19
dianaow <diana.ow@outlook.com>
2023-10-09 00:34:11 +0800

    test deployment to heroku

---
+++ main.py
@@ -0,0 +1,3 @@
+from server.app import app                                                               
+if __name__ == "__main__":                                             
+    app.run(debug=True)
